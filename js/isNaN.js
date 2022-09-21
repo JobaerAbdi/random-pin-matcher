53c52f242bf45103555fc736eec7d1bd4fb925a5
JobaerAbdi <jobaerrahman757@gmail.com>
2022-09-21 13:23:41 +0600

    Push = 01

---
+++ js/isNaN.js
@@ -0,0 +1,10 @@
+const strNumber = '4561';
+const numNumber = 7891;
+const notNumber = asd;
+
+if(isNaN(strNumber)){
+    console.log('This is string number');
+}
+else{
+    console.log('This is not a number');
+}
